ae2dc1559f55dc695c782df6badb58c1b6805668
utkarshbhadauria03 <136165956+utkarshbhadauria03@users.noreply.github.com>
2024-11-28 16:26:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const searchicon2=document.querySelector('#searchicon2');
+const srchicon2=document.querySelector('#srchicon2');
+const search2=document.querySelector('#searchinput2');
+
+searchicon2.addEventListener('click',function(){
+    search2.style.display='flex';
+    searchicon2.style.display='none';
+})
+
+
+
+
